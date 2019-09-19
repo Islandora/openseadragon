76b1c454e4b7de1873bcd6e532c87cf7099dfd9e
dannylamb <dlamb@islandora.ca>
2019-09-19 12:11:50 +0000

    Typo fix and providing link to available tokens

--- src/Plugin/Block/OpenseadragonBlock.php
+++ src/Plugin/Block/OpenseadragonBlock.php
@@ -26,16 +26,23 @@ class OpenseadragonBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function blockForm($form, FormStateInterface $form_state) {
-    $form['iiif_manifest_url'] = [
-      '#type' => 'textfield',
+    $form['iiif_manifest_url_fieldset'] = [
+      '#type' => 'fieldset',
       '#title' => $this->t('IIIF Manifest URL'),
-      '#description' => $this->t('Absolute URL of the IIIF manifest to render.  You may use tokens to provide a pattern (e.g. "http://localhost/node/[node:nid]/manifest"'),
+    ];
+    $form['iiif_manifest_url_fieldset']['iiif_manifest_url'] = [
+      '#type' => 'textfield',
+      '#description' => $this->t('Absolute URL of the IIIF manifest to render.  You may use tokens to provide a pattern (e.g. "http://localhost/node/[node:nid]/manifest")'),
       '#default_value' => $this->configuration['iiif_manifest_url'],
       '#maxlength' => 256,
       '#size' => 64,
-      '#weight' => '0',
       '#required' => TRUE,
     ];
+    $form['iiif_manifest_url_fieldset']['token_help'] = array(
+      '#theme' => 'token_tree_link',
+      '#token_types' => array('node'),
+    );
+
 
     return $form;
   }
