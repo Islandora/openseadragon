7ccc72e28749f24509601f4d0e07872c5e294452
Jonathan Hunt <git@huntdesign.co.nz>
2026-05-22 11:26:38 +1200

    #72 Normalise should map to boolean

--- src/Form/OpenSeadragonSettingsForm.php
+++ src/Form/OpenSeadragonSettingsForm.php
@@ -1073,9 +1073,11 @@ class OpenSeadragonSettingsForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $config = $this->configFactory->getEditable('openseadragon.settings');
-    $this->normalizeSettings($form_state->getValue('openseadragon_settings'));
+
     // Get default to match array formatting.
     $default_settings = $this->seadragonConfig->getDefaultSettings();
+
+    $this->normalizeSettings($form_state->getValue('openseadragon_settings'), $default_settings);
     $this->filterSettings($form_state->getValue('openseadragon_settings'), $default_settings);
     $config->set('viewer_options', $form_state->getValue('openseadragon_settings'));
 
@@ -1139,9 +1141,12 @@ class OpenSeadragonSettingsForm extends ConfigFormBase {
    * @return array
    *   Normalized settings.
    */
-  private function normalizeSettings(array &$settings) {
+  private function normalizeSettings(array &$settings, $defaultSettings) {
     foreach ($settings as $key => $value) {
-      $settings[$key] = $this->normalizeSetting($value);
+      // Some settings keys are not present in default settings, e.g. navigatorOptions.
+      if (!is_null($defaultSettings) && isset($defaultSettings[$key])) {
+        $settings[$key] = $this->normalizeSetting($value, $defaultSettings[$key]);
+      }
     }
     return $settings;
   }
@@ -1152,14 +1157,17 @@ class OpenSeadragonSettingsForm extends ConfigFormBase {
    * @param mixed $value
    *   The setting to be normalized.
    *
-   * @return array|float|int|string
+   * @return array|boolean|float|int|string
    *   The normalized setting.
    */
-  private function normalizeSetting($value) {
+  private function normalizeSetting($value, $defaultSetting) {
     if (is_array($value)) {
-      return $this->normalizeSettings($value);
+      return $this->normalizeSettings($value, $defaultSetting);
     }
     elseif (filter_var($value, FILTER_VALIDATE_INT) !== FALSE) {
+      if (is_bool($defaultSetting)) {
+        return (boolean) $value;
+      }
       return (int) $value;
     }
     elseif (filter_var($value, FILTER_VALIDATE_FLOAT) !== FALSE) {
